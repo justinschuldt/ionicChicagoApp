5ba2c23e69b61ba7f87d8a2c07c2a0e293af36d8
Justin Schuldt <justinschuldt@gmail.com>
2016-05-12 14:10:42 -0500

    added pages and removed defaults

---
+++ app/pages/capture/capture.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the CapturePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/capture/capture.html',
+})
+export class CapturePage {
+  constructor(public nav: NavController) {}
+}
---
+++ app/pages/feed/feed.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the FeedPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/feed/feed.html',
+})
+export class FeedPage {
+  constructor(public nav: NavController) {}
+}
