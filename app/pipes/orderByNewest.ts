3b7ef6c3e69075ab5a725a963f523b142c7d7e5f
Justin Schuldt <justinschuldt@gmail.com>
2016-05-13 13:38:02 -0500

    orderBy pipe

---
+++ app/pipes/orderByNewest.ts
@@ -0,0 +1,21 @@
+import {Injectable, Pipe} from 'angular2/core';
+
+/*
+  Generated class for the OrderByNewest pipe.
+
+  See https://angular.io/docs/ts/latest/guide/pipes.html for more info on
+  Angular 2 Pipes.
+*/
+@Pipe({
+  name: 'order-by-newest'
+})
+@Injectable()
+export class OrderByNewest {
+  /*
+    Takes a value and makes it lowercase.
+   */
+  transform(value: string, args: any[]) {
+    value = value + ''; // make sure it's a string
+    return value.toLowerCase();
+  }
+}
